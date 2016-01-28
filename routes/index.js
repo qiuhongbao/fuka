1ca65d1789a458f3623949029a363055fb158468
junxue.rao <junxue.rao@alipay.com>
2016-01-28 13:57:10 +0800

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '支付宝福卡--第三方交易市场' });
 });
 
 module.exports = router;
